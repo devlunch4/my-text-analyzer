0fec872b49d08863c7b06e409cdba900b64e7e86
Michael(devlunch4) <devlunch4@gmail.com>
2025-02-20 16:24:06 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TextAnalyzer from './components/TextAnalyzer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Text Analyzer</h1>
+      <TextAnalyzer />
+    </div>
+  );
+}
+
+export default App;
